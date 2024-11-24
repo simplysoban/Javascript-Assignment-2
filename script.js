b77643adc5542ddcf3b20b48f41f2d7eb175f6b6
“soban <sobansaleem713@gmail.com>
2024-11-24 12:31:59 +0500

    modified script.js

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ var one = prompt("Enter a positive floating number: ");
 
 console.log("POSITIVE:")
 
-console.log("Your number: ", one);
-console.log("Round off value: ", Math.round(one));
-console.log("Floor value: ", Math.floor(one));
-console.log("Ceil value: ", Math.ceil(one));
+if(one>0){
+    console.log("Your number: ", one);
+    console.log("Round off value: ", Math.round(one));
+    console.log("Floor value: ", Math.floor(one));
+    console.log("Ceil value: ", Math.ceil(one));
+}
+else{
+    console.log("Please enter a positive number.");
+}
+
 
 
 console.log("NEGATIVE:")
 
 var two = prompt("Enter a negative floating number: ");
 
-console.log("Your number: ", two);
-console.log("Round off value: ", Math.round(two));
-console.log("Floor value: ", Math.floor(two));
-console.log("Ceil value: ", Math.ceil(two));
+if(two<0){
+    console.log("Your number: ", two);
+    console.log("Round off value: ", Math.round(two));
+    console.log("Floor value: ", Math.floor(two));
+    console.log("Ceil value: ", Math.ceil(two));
+}
+else{
+    console.log("Please enter a negative number.");
+}
