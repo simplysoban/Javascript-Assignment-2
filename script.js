307a3af667475fce7c6abaabea2504229c67b3cd
“soban <sobansaleem713@gmail.com>
2024-11-24 12:17:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var one = prompt("Enter a positive floating number: ");
+
+console.log("POSITIVE:")
+
+console.log("Your number: ", one);
+console.log("Round off value: ", Math.round(one));
+console.log("Floor value: ", Math.floor(one));
+console.log("Ceil value: ", Math.ceil(one));
+
+
+console.log("NEGATIVE:")
+
+var two = prompt("Enter a negative floating number: ");
+
+console.log("Your number: ", two);
+console.log("Round off value: ", Math.round(two));
+console.log("Floor value: ", Math.floor(two));
+console.log("Ceil value: ", Math.ceil(two));
